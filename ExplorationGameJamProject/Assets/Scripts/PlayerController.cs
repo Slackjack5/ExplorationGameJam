b0da080f5609326b4bf6283cba4789ec89f3755c
Aljon Pineda <aljonjbpineda@gmail.com>
2021-06-12 21:42:56 -0400

    Move camera to first-person view

--- ExplorationGameJamProject/Assets/Scripts/PlayerController.cs
+++ ExplorationGameJamProject/Assets/Scripts/PlayerController.cs
@@ -5,13 +5,20 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    // Unity Editor fields
+    [SerializeField] private Camera playerCamera;
     [SerializeField] private float baseSpeed = 1000f;
+    [SerializeField] private float lookSensitivity = 100f;
     [SerializeField] private float movementSmoothTime = 0.4f;
 
+    // Private properties
+    private float cameraRotationX;
+    private float lookInputX;
+    private float lookInputY;
+    private float moveInputX;
+    private float moveInputZ;
     private Rigidbody rb;
     private Vector3 velocity;
-    private float xInput;
-    private float zInput;
 
     // Start is called before the first frame update
     void Start()
@@ -22,21 +29,36 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // Look
+        float targetHorizontalLook = lookSensitivity * lookInputX * Time.deltaTime;
+        transform.Rotate(Vector3.up * targetHorizontalLook);
+
+        float targetVerticalLook = lookSensitivity * lookInputY * Time.deltaTime;
+        cameraRotationX -= targetVerticalLook;
+        cameraRotationX = Mathf.Clamp(cameraRotationX, -90f, 90f);
+        playerCamera.transform.localRotation = Quaternion.Euler(cameraRotationX, 0f, 0f);
     }
 
     private void FixedUpdate()
     {
-        float targetVelocityX = baseSpeed * xInput * Time.fixedDeltaTime;
-        float targetVelocityZ = baseSpeed * zInput * Time.fixedDeltaTime;
+        // Move
+        float targetVelocityX = baseSpeed * moveInputX * Time.fixedDeltaTime;
+        float targetVelocityZ = baseSpeed * moveInputZ * Time.fixedDeltaTime;
         Vector3 targetVelocity = new Vector3(targetVelocityX, rb.velocity.y, targetVelocityZ);
         rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref velocity, movementSmoothTime);
+    }
+
+    public void OnLook(InputValue value)
+    {
+        Vector2 lookVector = value.Get<Vector2>();
+        lookInputX = lookVector.x;
+        lookInputY = lookVector.y;
     }
 
     public void OnMove(InputValue value)
     {
         Vector2 motionVector = value.Get<Vector2>();
-        xInput = motionVector.x;
-        zInput = motionVector.y;
+        moveInputX = motionVector.x;
+        moveInputZ = motionVector.y;
     }
 }
