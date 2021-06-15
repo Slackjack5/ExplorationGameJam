7db98ee2388aec7eacf08e74b72496548ae07c9d
Aljon Pineda <aljonjbpineda@gmail.com>
2021-06-14 20:09:37 -0400

    Fix CameraScene

--- ExplorationGameJamProject/Assets/Scripts/GameCamera.cs
+++ ExplorationGameJamProject/Assets/Scripts/GameCamera.cs
@@ -4,42 +4,42 @@ using UnityEngine;
 
 public class GameCamera : MonoBehaviour
 {
-    // Unity Editor fields
-    [SerializeField] private GameObject cameraCanvas;
+  // Unity Editor fields
+  [SerializeField] private GameObject cameraCanvas;
 
-    // Private properties
-    private int pictureCount;
+  // Private properties
+  private int pictureCount;
 
-    // Constants
-    private const string pathPrefix = "Screenshots/";
+  // Constants
+  private const string pathPrefix = "Screenshots/";
 
-    // Start is called before the first frame update
-    void Start()
+  // Start is called before the first frame update
+  void Start()
+  {
+    cameraCanvas.SetActive(false);
+  }
+
+  public void TakePicture()
+  {
+    if (cameraCanvas.activeSelf)
     {
-        cameraCanvas.SetActive(false);
+      // Disable the camera UI so that it doesn't appear in screenshot
+      cameraCanvas.SetActive(false);
+      StartCoroutine(CaptureFrame());
     }
+  }
 
-    public void TakePicture()
-    {
-        if (cameraCanvas.activeSelf)
-        {
-            // Disable the camera UI so that it doesn't appear in screenshot
-            cameraCanvas.SetActive(false);
-            StartCoroutine(CaptureFrame());
-        }
-    }
+  public void Toggle()
+  {
+    cameraCanvas.SetActive(!cameraCanvas.activeSelf);
+  }
 
-    public void Toggle()
-    {
-        cameraCanvas.SetActive(!cameraCanvas.activeSelf);
-    }
+  private IEnumerator CaptureFrame()
+  {
+    yield return new WaitForEndOfFrame();
 
-    private IEnumerator CaptureFrame()
-    {
-        yield return new WaitForEndOfFrame();
-
-        ScreenCapture.CaptureScreenshot(pathPrefix + pictureCount + ".png");
-        pictureCount++;
-        cameraCanvas.SetActive(true);
-    }
+    ScreenCapture.CaptureScreenshot(pathPrefix + pictureCount + ".png");
+    pictureCount++;
+    cameraCanvas.SetActive(true);
+  }
 }
