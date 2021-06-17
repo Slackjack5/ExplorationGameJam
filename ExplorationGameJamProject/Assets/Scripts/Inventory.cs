71538aea214e68a50e98cef5d0f69eef2600cc46
Aljon Pineda <aljonjbpineda@gmail.com>
2021-06-16 22:47:57 -0400

    Expand photo when selected

--- ExplorationGameJamProject/Assets/Scripts/Inventory.cs
+++ ExplorationGameJamProject/Assets/Scripts/Inventory.cs
@@ -7,9 +7,12 @@ public class Inventory : MonoBehaviour
 {
   [SerializeField] private GameObject inventoryPanel;
   [SerializeField] private GameObject sidebar;
+  [SerializeField] private GameObject selectedPhotoPanel;
   [SerializeField] private float photoShrinkFactor = 0.07f;
+  [SerializeField] private float selectedPhotoShrinkFactor = 0.5f;
 
-  private List<Sprite> photos;
+  private List<GameObject> photos;
+  private GameObject selectedPhoto;
 
   public bool IsOpen {
     get { return inventoryPanel.activeSelf; }
@@ -17,20 +20,23 @@ public class Inventory : MonoBehaviour
 
   private void Start()
   {
-    photos = new List<Sprite>();
+    photos = new List<GameObject>();
 
     inventoryPanel.SetActive(false);
   }
 
-  public void AddPhoto(Sprite photo)
+  public void AddPhoto(Sprite photoSprite)
   {
-    GameObject newObject = new GameObject();
-    Image newImage = newObject.AddComponent<Image>();
-    newImage.sprite = photo;
+    GameObject photo = new GameObject();
+    Image image = photo.AddComponent<Image>();
+    image.sprite = photoSprite;
 
-    RectTransform rectTransform = newObject.GetComponent<RectTransform>();
+    Button button = photo.AddComponent<Button>();
+    button.onClick.AddListener(() => Select(photo));
+
+    RectTransform rectTransform = photo.GetComponent<RectTransform>();
     rectTransform.SetParent(sidebar.transform);
-    rectTransform.sizeDelta = photoShrinkFactor * new Vector2(photo.texture.width, photo.texture.height);
+    rectTransform.sizeDelta = photoShrinkFactor * new Vector2(photoSprite.texture.width, photoSprite.texture.height);
 
     photos.Add(photo);
   }
@@ -47,5 +53,24 @@ public class Inventory : MonoBehaviour
     {
       Cursor.lockState = CursorLockMode.Locked;
     }
+  }
+
+  private void Select(GameObject photo)
+  {
+    if (selectedPhoto != null)
+    {
+      Destroy(selectedPhoto);
+    }
+
+    selectedPhoto = Instantiate(photo);
+    selectedPhoto.GetComponent<Button>().enabled = false;
+
+    Image image = selectedPhoto.GetComponent<Image>();
+    Sprite photoSprite = image.sprite;
+
+    RectTransform rectTransform = selectedPhoto.GetComponent<RectTransform>();
+    rectTransform.SetParent(selectedPhotoPanel.transform);
+    rectTransform.localPosition = Vector2.zero;
+    rectTransform.sizeDelta = selectedPhotoShrinkFactor * new Vector2(photoSprite.texture.width, photoSprite.texture.height);
   }
 }
