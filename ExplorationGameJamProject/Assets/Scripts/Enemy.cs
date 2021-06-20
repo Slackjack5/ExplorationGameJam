93d5378cecb0b7441d714f18ddcc45fd55c0f333
Zach <zlschect@gmail.com>
2021-06-20 13:33:16 -0400

    Added in an aggressiveness value for enemy
    Aggressiveness scales four things:
    -Particle intensity
    -Particle count
    -Wobble speed
    -Movement speed

--- ExplorationGameJamProject/Assets/Scripts/Enemy.cs
+++ ExplorationGameJamProject/Assets/Scripts/Enemy.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class Enemy : MonoBehaviour
 {
   [SerializeField] private List<Vector3> respawnPositions;
-
+  public float aggro = 1;
   public GameObject followObject;
-  public float speed;
+  public Material enemyMaterial;
   private bool isActivated;
   private Vector3 lookDirection;
+  private VisualEffect visualEffect;
 
   private void Start()
   {
+    visualEffect = GetComponent<VisualEffect>();
+    visualEffect.SetFloat("SpawnRate", aggro);
+    enemyMaterial.SetFloat("_WobbleSpeed", aggro / 10 + (aggro - 1) * 0.2f);
     isActivated = true;
   }
 
@@ -20,8 +25,15 @@ public class Enemy : MonoBehaviour
   {
     if (isActivated)
     {
+      visualEffect.SetFloat("SpawnRate", aggro);
+      enemyMaterial.SetFloat("_WobbleSpeed", aggro / 10 + (aggro - 1) * 0.2f);
       lookDirection = Vector3.Normalize(followObject.transform.position - transform.position);
-      transform.Translate(lookDirection * Time.deltaTime * speed);
+      transform.Translate(lookDirection * Time.deltaTime * aggro);
+    }
+    else
+    {
+      visualEffect.SetFloat("SpawnRate", 0);
+      enemyMaterial.SetFloat("_WobbleSpeed", 0);
     }
   }
 
