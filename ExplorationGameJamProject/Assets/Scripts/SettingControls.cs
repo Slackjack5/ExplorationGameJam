146810cd76e056cc0bc91a4489f516af7068cc49
Zach <zlschect@gmail.com>
2021-06-21 16:11:13 -0400

    Added in wobbling to shader at edges

---
+++ ExplorationGameJamProject/Assets/Scripts/SettingControls.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolumeController : MonoBehaviour
+{
+  void OnSensitivityMove(float value)
+  {
+      
+  }
+}
