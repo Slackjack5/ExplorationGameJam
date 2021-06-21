8510165d5fe0eb4f8c6d7e400487828cc06f47eb
Aljon Pineda <aljonjbpineda@gmail.com>
2021-06-20 21:07:58 -0400

    Add checkmark to memory photos

--- ExplorationGameJamProject/Assets/Scripts/Photo.cs
+++ ExplorationGameJamProject/Assets/Scripts/Photo.cs
@@ -6,10 +6,12 @@ public class Photo
 {
   public GameObject photoObject { get; }
   public Vector3 location { get; }
+  public bool isMemory { get; }
 
-  public Photo(GameObject photoObject, Vector3 location)
+  public Photo(GameObject photoObject, Vector3 location, bool isMemory)
   {
     this.photoObject = photoObject;
     this.location = location;
+    this.isMemory = isMemory;
   }
 }
